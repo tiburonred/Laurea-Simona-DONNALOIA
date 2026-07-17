8bb3ec22f7b2c8bc838e1704f284c181aad1d4f9
tiburonred <tiburonred@yahoo.it>
2026-07-17 12:05:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("Laurea di Simona pronta!");
+const cloudName = "xcc0isj0";
+const uploadPreset = "laurea-simona";
+
+function caricaFoto() {
+
+    const files = document.getElementById("uploadFoto").files;
+
+    if (files.length === 0) {
+        document.getElementById("messaggioUpload").innerHTML =
+        "Seleziona prima una foto";
+        return;
+    }
+
+    for (let i = 0; i < files.length; i++) {
+
+        let formData = new FormData();
+
+        formData.append("file", files[i]);
+        formData.append("upload_preset", uploadPreset);
+
+        fetch(
+        https://api.cloudinary.com/v1_1/${cloudName}/image/upload,
+        {
+            method: "POST",
+            body: formData
+        }
+        )
+        .then(response => response.json())
+        .then(data => {
+            console.log("Foto caricata:", data.secure_url);
+
+            document.getElementById("messaggioUpload").innerHTML =
+            "✅ Foto caricata con successo!";
+        })
+        .catch(error => {
+            console.error(error);
+            document.getElementById("messaggioUpload").innerHTML =
+            "Errore durante il caricamento";
+        });
+
+    }
+}
